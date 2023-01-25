40ebcb0f6175cc24333f212b6c974c6bb86471c0
Da Eun Ha <hekdms0429@gmail.com>
2023-01-25 15:58:01 +0900

    3주차

--- 3주차/square.py
+++ 3주차/square.py
@@ -1,9 +1,16 @@
 import turtle
-t=turtle.Turtle()
-t.shape("turtle")
 
-i=0
-while i<4:
-    t.forward(100)
-    t.left(90)
-    i=i+1
+
+def main():
+    t = turtle.Turtle()
+    t.shape("turtle")
+
+    i = 0
+    while i < 4:
+        t.forward(100)
+        t.left(90)
+        i = i + 1
+
+
+if __name__ == '__main__':
+    main()
--- 3주차/star.py
+++ 3주차/star.py
@@ -1,8 +1,14 @@
 import turtle
-t= turtle.Turtle()
-t.shape("turtle")
-i=0
-while i<5:
-    t.forward(100)
-    t.left(144)
-    i=i+1
+
+def main():
+    t = turtle.Turtle()
+    t.shape("turtle")
+    i = 0
+    while i < 5:
+        t.forward(100)
+        t.left(144)
+        i = i + 1
+
+
+if __name__ == '__main__':
+    main()
