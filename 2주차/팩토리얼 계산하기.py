672823c8618c5cec2e48fc1828acda6c67ae956d
Da Eun Ha <hekdms0429@gmail.com>
2023-02-03 13:07:13 +0900

    cancel

--- 2주차/팩토리얼 계산하기.py
+++
@@ -1,5 +0,0 @@
-n=int(input("정수를 입력하시오: "))
-fact=1
-for i in range(1, n+1):
-    fact = fact * i
-print(n, "!은", fact, "이다.")
--- 4주차/add.py
+++
@@ -1,7 +0,0 @@
-def main():
-    sum=0
-    for i in range(1,11):
-        sum+=i
-    print(sum)
-if __name__ == '__main__':
-    main()
