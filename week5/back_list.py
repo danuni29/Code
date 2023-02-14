00a24e83d056aa4e31b4878bbe1b91e70b4c5f52
Daeun Ha <hekdms0429@gmail.com>
2023-02-14 10:34:14 +0900

    week5

--- week5/back_list.py
+++ week5/back_list.py
@@ -2,10 +2,16 @@ def range_list(n, x):
     for i in range(0, n+1):
         print(x[i])
 
+
+def range_list2(n):
+    results = []
+    for i in range(n):
+        # results.append(i)
+        print(i)
+
 def main():
     n=int(input("입력하세요"))
-    x = [3, 7, 25, 10, 2, 13]
-    range_list(n, x)
+    range_list2(n)
 
 if __name__ == '__main__':
     main()
